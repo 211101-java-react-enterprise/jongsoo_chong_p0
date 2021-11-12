42136b46720932afaca22c0cb3c483e3ed407276
cjsssusa <cjsssusa@gmail.com>
2021-11-11 22:03:52 -0500

    renew source

--- src/main/java/com/revature/quizzard/services/UserService.java
+++ src/main/java/com/revature/quizzard/services/UserService.java
@@ -8,7 +8,17 @@ import com.revature.quizzard.models.AppUser;
 
 public class UserService {
 
-    private AppUserDAO userDAO = new AppUserDAO();
+    private final AppUserDAO userDAO ;
+    private AppUser sessionUser;
+
+    public UserService(AppUserDAO userDAO) {
+        this.userDAO = userDAO;
+        this.sessionUser = null;
+    }
+
+    public AppUser getSessionUser() {
+        return sessionUser;
+    }
 
     public boolean registerNewUser(AppUser newUser) {
 
@@ -16,7 +26,22 @@ public class UserService {
             throw new InvalidRequestException("Invalid user data provided!");
         }
 
-        // TODO: write logic that verifies that the new user's username and email are not already taken
+        boolean usernameAvailable = userDAO.findUserByUsername(newUser.getUsername()) == null;
+        boolean emailAvailable = userDAO.findUserByEmail(newUser.getEmail()) == null;
+
+        if (!usernameAvailable || !emailAvailable) {
+//            if (!usernameAvailable && emailAvailable) {
+//                throw new ResourcePersistenceException("The provided username was already taken in the datasource!");
+//            } else if (usernameAvailable) {
+//                throw new ResourcePersistenceException("The provided email was already taken in the datasource!");
+//            } else {
+//                throw new ResourcePersistenceException("The provided username and email was already taken in the datasource!");
+//            }
+            String msg = "The values provided for the following fields are already taken by other users:";
+            if (!usernameAvailable) msg = msg + "\n\t- username";
+            if (!emailAvailable) msg = msg + "\n\t- email";
+            throw new ResourcePersistenceException(msg);
+        }
 
         AppUser registeredUser = userDAO.save(newUser);
 
@@ -28,7 +53,7 @@ public class UserService {
 
     }
 
-    public AppUser authenticateUser(String username, String password) {
+    public void authenticateUser(String username, String password) {
 
         if (username == null || username.trim().equals("") || password == null || password.trim().equals("")) {
             throw new InvalidRequestException("Invalid credential values provided!");
@@ -40,8 +65,16 @@ public class UserService {
             throw new AuthenticationException();
         }
 
-        return authenticatedUser;
+        sessionUser = authenticatedUser;
 
+    }
+
+    public void logout() {
+        sessionUser = null;
+    }
+
+    public boolean isSessionActive() {
+        return sessionUser != null;
     }
 
     public boolean isUserValid(AppUser user) {
